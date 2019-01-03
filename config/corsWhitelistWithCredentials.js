488f5c1bfbb0b2a36000e15480fe5211f3e4d553
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-01-03 12:54:38 +0100

    adds http://localhost:3000 to cors origin whitelist

--- config/corsWhitelistWithCredentials.js
+++ config/corsWhitelistWithCredentials.js
@@ -1,3 +1,6 @@
 require('dotenv').config()
 
-module.exports = { origin: process.env.CLIENT_URL, credentials: true }
+module.exports = {
+  origin: [process.env.CLIENT_URL, 'http://localhost:3000'],
+  credentials: true
+}
