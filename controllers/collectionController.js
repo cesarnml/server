ed41b59f53c65257d7667313e9d0642ced3c426a
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-01-04 00:57:17 +0100

    adds getCollectionById resource

--- controllers/collectionController.js
+++ controllers/collectionController.js
@@ -8,4 +8,12 @@ const crudRouteMaker = require('../helpers/crudRouteMaker')
 const crudMethods = crudMethodMaker(db, 'collections')
 crudRouteMaker(router, crudMethods)
 
+router.get('/:id', getCollectionById)
+
+function getCollectionById (req, res, next) {
+  const { id } = req.params
+  db('collections')
+    .where({ id })
+    .then(collection => res.status(200).json(collection))
+}
 module.exports = router
